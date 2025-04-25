8b5d580186f3050f3799f44b5f45018c73b9c76b
Rafael Castro <ralphc1992@live.com>
2025-04-25 12:01:12 -0500

    updating site

--- src/pages/Devnetiks.tsx
+++ src/pages/Devnetiks.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from "react"
-import { FaAngleDoubleDown } from "react-icons/fa";
+import { useEffect, useRef, useState } from "react";
 import logo from "../assets/devnetiks-white.svg";
-import computer from "../assets/computer.jpg";
+import computer from "../assets/pc.svg";
 import rafita from "../assets/rafita.jpg";
 import rosebg from "../assets/rosebg.svg";
+import aboutbg from "../assets/aboutbg.svg";
 
 import reactLogo from "../assets/react.png";
 import firebaseLogo from "../assets/firebase.png";
@@ -94,12 +94,12 @@ export const Devnetiks: React.FC = () => {
     return <>
     {/* MAIN CONTAINER */}
 
-    <div className="relative">
+    <div className="relative w-full h-full">
           {/* FIXED SIDEBAR NAVIGATION */}
-          <div className="fixed px-4 z-50  right-6 top-30 md:top-1/2 transform -translate-y-1/2 flex flex-col space-y-4 text-gray-950">
+          <div className="fixed px-4 z-60  right-6 top-30 md:top-1/2 transform -translate-y-1/2 flex flex-col space-y-4">
         {sections.map((id) => (
-            <a key={id} href={`#${id}`} className={`text-lg transition text-right ${
-                activeSection === id ? ` font-bold animate-pulse ease-in-out duration-300 scale-120 ${sectionColors[id]}` : ' opacity-30'
+            <a key={id} href={`#${id}`} className={`text-lg transition text-right hover:scale-110 duration-300 ${
+                activeSection === id ? ` font-bold animate-pulse ease-in-out duration-300 scale-120 ${sectionColors[id]}` : ' opacity-30 text-white'
             }`}>
                 {id}
             </a>
@@ -110,26 +110,24 @@ export const Devnetiks: React.FC = () => {
         <div>
 
             {/* HOME */}
-           <section id="home" className="relative w-full z-[40] h-[100vh] flex items-center justify-center">
+
+            {/* HOME SECTION CONTAINER */}
+           <section id="home" className="relative w-full z-[50] overflow-hidden h-[100vh] flex items-center justify-center">
+
+            {/* FLEX CONTAINER */}
             <div className="w-full h-full flex items-center justify-center relative px-10 md:px-0">
          <div>
           {/* LOGO */}
-        <img className={`transition ease-out delay-0 w-[250px] md:w-[500px] mb-2 duration-700 ${
+        <img className={`transition ease-out delay-0 w-[250px] md:w-[350px] mb-2 duration-700 ${
             activeSection === 'home' ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-[-2000px] '
         }`} src={logo} alt="" />
-       
-            <p className={`offwhite text-lg md:text-3xl transition delay-400 ease-out duration-700 ${
+              {/* SUBTEXT FOR LOGO */}
+            <p className={`offwhite text-lg md:text-2xl transition delay-400 ease-out duration-700 ${
                 activeSection === 'home' ? 'opacity-100 translate-y-0 delay-0' : 'opacity-0 translate-y-80'
             }`}>Building sleek, powerful websites tailored to your vision.</p>
          </div>
-        <div className="fixed z-[40] w-full h-[20px] bottom-10 left-0">
-       <a href="#about">
-       <FaAngleDoubleDown className={`bottom-0 mx-auto size-10 text-cyan-400 animate-bounce cursor-pointer transition duration-700 ${
-                activeSection === 'home' ? 'opacity-100 translate-y-0 delay-600' : 'hidden opacity-0 translate-y-20 delay-600'
-            }`} />
-       </a>
 
-        </div>
+        
             </div>
             {/* BACKGROUND IMAGE FOR HERO SECTION */}
             <div className="absolute z-[-1] overflow-hidden top-0 left-0 w-full h-full">
@@ -137,36 +135,43 @@ export const Devnetiks: React.FC = () => {
             </div>
            </section>
 
-           {/* ABOUT */}
-           <section id="about" className="relative z-40 h-[100vh] w-full flex items-center justify-center px-10">
+           {/* ABOUT SECTION*/}
+           <section id="about" className="relative z-40 min-h-screen w-full flex items-center justify-center px-4 md:px-10">
+
+            {/* MAIN CONTAINER FOR ABOUT SECTION */}
             <div className="w-full h-full flex items-center justify-center relative">
+
+              {/* CONTROLS THE MAX WIDTH OF 1200PX  */}
              <div className="w-full max-w-[1200px] relative mx-auto">
                 {/* NAV ITEMS */}
-                <div className="flex text-2xl md:text-4xl gap-5 justify-start transition duration-300 ease-out">
+                <div className="flex text-2xl md:text-3xl gap-5 justify-start transition duration-300 ease-out z-60">
+
+
+                  {/* NAV WHAT */}
   <h1
     onClick={() => setActiveTab('what')}
-    className={`cursor-pointer transition-all duration-500 delay-0  ease-in-out ${
-      activeSection === 'about' ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-[-200px]'
+    className={`relative z-50 cursor-pointer transition-all duration-800 delay-0  ease-in-out ${
+      activeSection === 'about' ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[-500px]'
     } ${activeTab === 'what' ? 'text-white' : 'text-gray-700'}
     `}
   >
     WHAT
   </h1>
-
+                {/* NAV WHY */}
   <h1
     onClick={() => setActiveTab('why')}
-    className={`cursor-pointer transition-all duration-500 delay-0 ease-in-out ${
-      activeSection === 'about' ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-[-200px]'
+    className={`cursor-pointer transition-all duration-800 delay-100 ease-in-out ${
+      activeSection === 'about' ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[500px]'
     } ${activeTab === 'why' ? 'text-white' : 'text-gray-700'}
     `}
   >
     WHY
   </h1>
-
+                {/* NAV HOW */}
   <h1
     onClick={() => setActiveTab('how')}
-    className={`cursor-pointer transition-all duration-500 delay-0 ease-in-out ${
-      activeSection === 'about' ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-[-200px]'
+    className={`cursor-pointer transition-all duration-800 delay-200 ease-in-out ${
+      activeSection === 'about' ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-[500px]'
     } ${activeTab === 'how' ? 'text-white' : 'text-gray-700'}
     `}
   >
@@ -174,6 +179,7 @@ export const Devnetiks: React.FC = () => {
   </h1>
 </div>
 
+{/* ABOUT CONTENT CONTAINER DISPLAYS BASED ON SELECTED NAV ITEM */}
 <div className={`transition duration-1200 ease-out delay-600 ${activeSection === 'about' ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[500px]'}`}>
 <div className="w-full  text-white mt-20 transition-all duration-700">
   <h1 className="text-2xl mb-2">
@@ -181,7 +187,7 @@ export const Devnetiks: React.FC = () => {
     {activeTab === 'why' && "WHY I DO IT"}
     {activeTab === 'how' && "HOW I DO IT"}
   </h1>
-  <p className={`text-2xl transition-all darkest py-5 duration-1000 text-emerald-800 ${activeTab === 'what' && 'translate-y-0'} ${activeTab === 'why' && 'translate-y-[20px]'} ${activeTab === 'how' && 'translate-y-[40px]'} `}>
+  <p className={`text-2xl transition-all darkest py-5 duration-1000 text-white ${activeTab === 'what' && 'translate-y-0'} ${activeTab === 'why' && 'translate-y-[20px]'} ${activeTab === 'how' && 'translate-y-[40px]'} `}>
   {activeTab === 'what' && <><div>
       <p className="mb-5">I build fast, interactive websites using React, TypeScript, and Tailwind CSS, backed by Firebase for powerful real-time functionality. Whether it's a business site or a user dashboard, I focus on delivering polished, scalable solutions that are responsive and clean.</p>
       <div className="flex gap-5">
@@ -218,25 +224,26 @@ export const Devnetiks: React.FC = () => {
           
 </div>
             {/* IMAGE CONTAINER */}
-            <div className="absolute z-[-1] h-full right-0 top-0 w-[400px]">
-            <img className={`filter grayscale object-cover transition ease-in-out duration-2000 delay-500  ${activeSection === 'about' ? ' scale-100 opacity-20 translate-x-0' : 'translate-x-[600px] opacity-0 scale-0'}`} src={rafita} alt="" />
+            <div className="hidden md:block absolute z-[-1] h-full right-0 top-0 w-[400px]">
+            <img className={`filter grayscale object-cover transition ease-in-out duration-2000 delay-500  ${activeSection === 'about' ? ' scale-100 opacity-20 lg:opacity-100 translate-x-0' : 'translate-x-[600px] opacity-0 scale-0'}`} src={rafita} alt="" />
             </div>
              </div>
             </div>
-            <div className="fixed bottom-5 w-full flex items-center justify-center ">
-            <a href="#services">
-       <FaAngleDoubleDown className={` size-10 text-emerald-400 animate-bounce cursor-pointer transition duration-700 ${
-                activeSection === 'about' ? 'opacity-100 translate-y-0 delay-600 block' : 'hidden opacity-0 translate-y-20'
-            }`} />
-       </a>
-            </div>
+
+              {/* ABSOLUTE BG IMAGE */}
+        <div className={`overflow-hidden fixed z-[-10] top-0 left-0 w-full h-full transition duration-1000 delay-200 ${activeSection === "about" ? 'opacity-30' : 'opacity-0'}`}>
+          <img className={`min-h-screen object-cover transition duration-700 delay-400 opacity-10 ${activeSection === "about" && 'scale-120'}`} src={aboutbg} alt="" />
+        </div>
            </section>
 
            {/* SERVICES */}
-           <section id="services" className="relative h-[100vh] flex items-center justify-center dark">
+           <section id="services" className="relative h-[100vh] w-full flex items-center justify-center dark">
            <div className="h-full w-full flex items-center justify-center">
          <div>
-         <h1 className={`text-6xl text-rose-400 mb-20 transition duration-700 sticky top-0 z-50 dark py-5 delay-300 ${activeSection === 'services' ? 'translate-y-0 opacity-100' : 'translate-y-[-300px] opacity-0'}`}>PACKAGES</h1>
+          {/* PACKAGES HEADER */}
+         <h1 className={`text-3xl md:text-4xl lg:text-6xl text-rose-400 mb-5 transition duration-800 sticky top-0 z-50 dark py-3 delay-300 ${activeSection === 'services' ? 'translate-y-0 opacity-100 animate-pulse' : 'translate-y-[-500px] opacity-0'}`}>PACKAGES</h1>
+         {/* PACKAGES SUBTEXT */}
+         <h2 className={`text-1xl md:text-2xl lg:text-4xl text-white mb-20 transition duration-800  z-50 delay-600 ${activeSection === 'services' ? 'translate-y-0 opacity-100' : 'translate-x-[-500px] opacity-0'}`}>ALL PACKAAGES ARE BUILT WITH REACT + TYPESCRIPT</h2>
            <div className="flex flex-col lg:flex-row gap-5 w-full max-w-[1400px]">
            {services.map((service, index) => {
   const delay = 300 + index * 150; // delay per card
@@ -251,10 +258,10 @@ export const Devnetiks: React.FC = () => {
           : 'translate-x-[-1500px] opacity-0'
       }`}
     >
-      <h1 className="mb-5 text-white text-4xl">{service.name}</h1>
+      <h1 className={`mb-5 text-white text-4xl transition-all duration-700 ${activeSection === 'services' ? 'translate-x-0' : 'translate-x-[500px]'}`}>{service.name}</h1>
       <h2 className="my-5 text-xl dtext-white">Ideal for: {service.ideal}</h2>
       {service.content}
-      <p className="my-5 text-lg w-[200px] bg-rose-400 px-5 text-gray-950 font-bold">
+      <p className="animate-pulse my-5 text-lg w-[200px] bg-rose-400 px-5 text-gray-950 font-bold">
         Price: {service.price}
       </p>
     </div>
@@ -266,8 +273,8 @@ export const Devnetiks: React.FC = () => {
          </div>
         </div>
         {/* ABSOLUTE BG IMAGE */}
-        <div className={`fixed z-10 top-0 left-0 w-full h-full transition duration-1000 delay-400 ${activeSection === "services" ? 'opacity-80' : 'opacity-0'}`}>
-          <img className="min-h-screen object-cover" src={rosebg} alt="" />
+        <div className={`fixed z-10 top-0 left-0 w-full h-full transition duration-1000 delay-200 ${activeSection === "services" ? 'opacity-80' : 'opacity-0'}`}>
+          <img className={`min-h-screen object-cover transition duration-700 delay-400 ${activeSection === "services" && 'animate-pulse scale-120'}`} src={rosebg} alt="" />
         </div>
            </section>
 
