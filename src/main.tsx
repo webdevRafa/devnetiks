b5557500a7c251a5306cda224c33a1946e9fd039
Rafael Castro <ralphc1992@live.com>
2025-08-08 13:43:46 -0500

    wowwwww

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// Import the router
+import AppRouter from "./routes/AppRouter"; // adjust path if no @ alias
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppRouter />
+  </StrictMode>
+);
